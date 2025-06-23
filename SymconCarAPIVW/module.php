a4634e34c04ae8664f0ce0e5caa5aa8e637f0c9e
pesensie <70333661+pesensie@users.noreply.github.com>
2025-06-23 09:58:55 +0000

    initial commit v0.1

--- SymconCarAPIVW/module.php
+++ SymconCarAPIVW/module.php
@@ -19,6 +19,26 @@ class SymconCarAPIVW extends IPSModule
         $username = $this->ReadPropertyString('Username');
         $password = $this->ReadPropertyString('Password');
 
+        $token = $this->LoginAndGetToken($username, $password);
+
+        if ($token === false) {
+            $this->SendDebug('Login', 'Verbindung fehlgeschlagen.', 0);
+            return;
+        }
+
+        $this->SendDebug('Login', 'Verbindung erfolgreich, Token erhalten.', 0);
+
+        $list = $this->SendAPIRequest('/garage', $token);
+
+        if (isset($list['children'])) {
+            $this->SendDebug('Fahrzeugliste', print_r($list['children'], true), 0);
+        } else {
+            $this->SendDebug('Fahrzeugliste', 'Keine Daten erhalten oder ungültige Antwort.', 0);
+        }
+    }
+
+    private function LoginAndGetToken($username, $password)
+    {
         $postFields = json_encode([
             'username' => $username,
             'password' => $password
@@ -39,9 +59,26 @@ class SymconCarAPIVW extends IPSModule
         $data = json_decode($response, true);
 
         if (isset($data['access_token'])) {
-            $this->SendDebug('Login', 'Verbindung erfolgreich hergestellt.', 0);
-        } else {
-            $this->SendDebug('Login', 'Verbindung fehlgeschlagen: ' . print_r($data, true), 0);
+            return $data['access_token'];
         }
+
+        return false;
+    }
+
+    private function SendAPIRequest($endpoint, $accessToken)
+    {
+        $url = 'https://myvwid.apps.emea.vwapps.io/api/v2' . $endpoint;
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Authorization: Bearer $accessToken",
+            'Accept: application/json'
+        ]);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($response, true);
     }
 }
