940fe1b0fa176e30705b615cf3c4f24d57cf5ff4
pesensie <70333661+pesensie@users.noreply.github.com>
2025-06-23 13:08:24 +0000

    initial commit v0.1

--- CarAPIVW/module.php
+++ CarAPIVW/module.php
@@ -7,6 +7,7 @@ class CarAPIVW extends IPSModule
         parent::Create();
         $this->RegisterPropertyString('Username', '');
         $this->RegisterPropertyString('Password', '');
+        $this->RegisterPropertyString('Brand', 'vw');
     }
 
     public function ApplyChanges()
@@ -18,8 +19,9 @@ class CarAPIVW extends IPSModule
     {
         $username = $this->ReadPropertyString('Username');
         $password = $this->ReadPropertyString('Password');
+        $brand = $this->ReadPropertyString('Brand');
 
-        $token = $this->LoginAndGetToken($username, $password);
+        $token = $this->LoginAndGetToken($username, $password, $brand);
 
         if ($token === false) {
             $this->SendDebug('WeConnect', 'Verbindung fehlgeschlagen.', 0);
@@ -29,13 +31,15 @@ class CarAPIVW extends IPSModule
         $this->SendDebug('WeConnect', 'Verbindung erfolgreich, Token erhalten.', 0);
     }
 
-    private function LoginAndGetToken($username, $password)
+    private function LoginAndGetToken($username, $password, $brand)
     {
+        $clientId = $this->GetClientID($brand);
+
         $postFields = http_build_query([
             'grant_type' => 'password',
             'username' => $username,
             'password' => $password,
-            'client_id' => 'a24e9f36-1160-4b9f-9d34-52d54ffb82ea',
+            'client_id' => $clientId,
             'scope' => 'openid profile mbb dealers cars vin'
         ]);
 
@@ -57,5 +61,22 @@ class CarAPIVW extends IPSModule
         }
 
         return false;
+    }
+
+    private function GetClientID(string $brand): string
+    {
+        switch (strtolower($brand)) {
+            case 'audi':
+                return '7f3a14e1-3b42-4b82-94d6-ea9c00f3f9b4';
+            case 'skoda':
+                return 'cd2e2b3c-bad1-47b7-a0c3-4f1803bffb03';
+            case 'seat':
+                return 'd9e5ba2e-1d35-4e4c-9a98-7610d4c6e5e1';
+            case 'cupra':
+                return 'e84f3a06-5e4f-4a2e-9cf5-2bd94e924f25';
+            case 'vw':
+            default:
+                return 'f6e4d3c5-1f72-4a9b-ae4e-cab281c0f2ec';
+        }
     }
 }
